90bbbdb2295f389f4f268c3c9510a5012af01979
miguel.correopers@gmail.com <ma@vmi1170525.contaboserver.net>
2023-02-09 12:22:45 +0100

    Despliegue_prueba12_09/02/2023

--- pCine/build/install.sh
+++ pCine/build/install.sh
@@ -1,4 +1,4 @@
 echo "Desplegando la app de gstión de un Cine"
 
-docker-compose -f /build/docker-compose.yml build
-docker-compose -f /build/docker-compose.yml up -d
+docker-compose -f ./build/docker-compose.yml build
+docker-compose -f ./build/docker-compose.yml up -d
