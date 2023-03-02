3bd2d68bc9dc6f28a0f89f554418641ca5a70ce7
miguel.correopers@gmail.com <miguel.correopers@gmail.com>
2023-03-02 12:25:50 +0100

    Cambios8_02/03/2023

--- pCine/build/docker/apicine2022/install.sh
+++
@@ -1,4 +0,0 @@
-echo "Desplegando la app de gstión de un Cine"
-
-docker-compose -f pCine/build/docker/apicine2022/docker-compose.yml build
-docker-compose -f pCine/build/docker/apicine2022/docker-compose.yml up -d
